d40c7cdeeea229507ef20904837aecca9cd3e2a4
david <mddavid11204@gmail.com>
2022-11-24 21:25:52 +0530

    some models is are submit into final delvierables

---
+++ Visualizing and Predicting Heart Diseases/Final deliverables/Source Code/app.py
@@ -0,0 +1,11 @@
+# from flask import Flask,render_template,request
+
+# app = Flask(__name__)
+# @app.route('/',methods=['GET'])
+# def hello_world():
+#   return render_template('form.html')
+print('hello')
+
+
+
+
